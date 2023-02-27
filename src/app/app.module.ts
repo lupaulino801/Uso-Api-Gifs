7f863b6ce6a7dd436998fc9a9609c75657f10898
Lucas <elpent801@gmail.com>
2023-02-27 09:33:38 -0800

    Cuarta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    GifsModule
+    GifsModule,
+    HttpClientModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/servisGifs/gifs-services.service.ts
+++ src/app/gifs/servisGifs/gifs-services.service.ts
@@ -1,13 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsServicesService {
-
-  constructor() { }
-
+  private api_key = "2vnUhZg20d46FDGQ8RZDMFnzAnw3KflO";
   private _hisorial : string [] = [];
+
+
+  constructor(private http: HttpClient) { }
+
+
+ 
 
   getHistorial(){
     return [...this._hisorial];
@@ -19,14 +24,22 @@ export class GifsServicesService {
 
     if(!this._hisorial.includes(query)){
       if(this._hisorial.length < 4){
-        this._hisorial.unshift(query);
+      this._hisorial.unshift(query);
       }else{
         console.log("Ya gastaste tus limites de add")
       }
 
     }
 
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.api_key}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+   
+
   }
+
+ 
+
+
+  
   
 
 }
